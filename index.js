a09fbf0a0887730cddda5d959683757b3452190d
C <C>
2024-12-03 13:48:42 -0800

    second

--- index.js
+++ index.js
@@ -21,3 +21,16 @@ document.addEventListener('mouseup', () => {
     isDragging = false;
     character.style.cursor = 'grab';
 });
+var buttons = document.getElementsById("container");
+
+button.onclick = onbuttonclicked;
+
+function onbuttonclicked(){
+    if (onbuttonclicked) {
+        button1.style.backgroundColor = "red";
+        button1.disabled=true;
+    } else {
+        button1.style.backgroundColor = "green";
+        button1.disabled=false;
+      }
+    }
