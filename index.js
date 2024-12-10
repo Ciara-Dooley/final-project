7060e67a797acf6bbe07d4afa748bca6eda8718f
C <C>
2024-12-10 13:40:54 -0800

    blahhh

--- index.js
+++ index.js
@@ -4,47 +4,62 @@ let count = 0
 
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1
+  countEl.textContent = count
+  checkPerfectScore();
 }
 
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+  let countStr = count + " - "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
 }
 
-    //new part in particlar the part about //
-    const questions = [
-                   "chose an emojie with a smile?",
-                    "pick an emojie an a eyeglasses?",
-                    "chose an emojie with horns ?",
-                    "chose an emojie with a monkey?",
-                    "chose an emojie with hearts?"
-                ];
-                let currentQuestionIndex = 0;
-                let questionInterval;
-                document.getElementById('showQuestionButton').addEventListener('click', function() {
-                    showQuestion();
-                    questionInterval = setInterval(changeQuestion, 10);
-                });
-                function showQuestion() {
-                    document.getElementById('question').innerText = questions[currentQuestionIndex];
-                }
-                function changeQuestion() {
-                    currentQuestionIndex = (currentQuestionIndex + .01) % questions.length;
-                    showQuestion();
-                }
+//new part in particlar the part about //
+const questions = [
+  "chose an emojie with a smile?",
+  "pick an emojie wich has eyeglasses?",
+  "chose an emojie with horns ?",
+  "chose an emojie with a monkey?",
+  "chose an emojie with hearts?"
+
+
+];
+
+let currentQuestionIndex = 0;
+let questionInterval;
+
+document.getElementById('showQuestionButton').addEventListener('click', function () {
+
+  showQuestion();
+
+
+  questionInterval = setInterval(changeQuestion, 15000);
+});
+
+function showQuestion() {
+  document.getElementById('question').innerText = questions[currentQuestionIndex];
+}
+
+function changeQuestion() {
+  currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
+  showQuestion();
+}
+
+
+
 //NEW PART//
-let perfect = document.getElementById("perfect")
 
+let perfectscore = document.getElementById("perfectscore")
 
- if (countEl == 50){
-  document.getElementById("perfect").style.display = "block";
- }
- else{
-  document.getElementById("perfect").style.display = "none";
- }
+function checkPerfectScore(){
+  if (count == 5) {
+    perfectscore.hidden = false;
+  }
+  else {
+    perfectscore.hidden = true;
+  }
+}
 
